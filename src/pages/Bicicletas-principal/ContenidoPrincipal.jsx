9363caecc015ea522e0843b14abc77f8d452898a
reyK06 <rlliiirollooo@gmail.com>
2025-03-18 21:26:07 -0500

    feat: agregar estructura inicial del main
    - Agregar imagen de fondo al main.
    - Implementar cards básicas con estructura inicial.
    - Falta trabajar en estilos y funcionalidad de las cards.

--- src/pages/Bicicletas-principal/ContenidoPrincipal.jsx
+++ src/pages/Bicicletas-principal/ContenidoPrincipal.jsx
@@ -1,9 +1,50 @@
 import React from 'react'
+import "./stylosprincipal.css"
 
 export default function ContenidoPrincipal() {
   return (
     <main>
-      <img src="/main/principal/principal-fondo.jpg" alt="" />
+      <section className='img-fondo'>
+        <article className='slogan'>
+          <h2>
+            Tu aventura comienza aquí. 
+          </h2>
+          <p>
+            Cada viaje en bicicleta es una nueva aventura. 
+            Desde la ciudad hasta la montaña, encuentra tu camino y
+            vive momentos inolvidables con tu Bici Ya.
+          </p>
+          <button>Ver</button>
+        </article>
+      </section>
+      <section className='marcas'>
+        <img src="/main/marcas/Colnago_logo.jpg" alt="" />
+        <img src="/main/marcas/Scott_Logo.png" alt="" />
+        <img src="/main/marcas/Trek_logo.png" alt="" />
+        <img src="/main/marcas/Giant_logo.png" alt="" />
+      </section>
+      <section class="proceso-bicicleta">
+        <h2>¡Tu bici está a solo 3 pasos!</h2>
+        <div class="pasos">
+          <div class="paso">
+            <div class="icono">🚴‍♂️</div>
+            <h3>Pide tu bici</h3>
+            <p>Elige la bicicleta de tus sueños y haz tu pedido en minutos.</p>
+          </div>
+          
+          <div class="paso">
+            <div class="icono">📦</div>
+            <h3>Te la llevamos</h3>
+            <p>Nos encargamos de que llegue a tu puerta de forma rápida y segura.</p>
+          </div>
+
+          <div class="paso">
+            <div class="icono">😊</div>
+            <h3>¡Sé feliz!</h3>
+            <p>Disfruta de tu nueva bici y vive aventuras increíbles.</p>
+          </div>
+        </div>
+      </section>
     </main>
   )
 }
